b4dde6c2509f1b086df7ae4bb999ce566719e67a
ttionya <git@ttionya.com>
2016-12-17 13:48:58 +0800

    update developing shell

--- developing/init.sh
+++ developing/init.sh
@@ -13,7 +13,12 @@ php artisan migrate
 
 php artisan db:seed
 
-# install global cnpm
-npm i cnpm -g --registry=https://registry.npm.taobao.org/
+# check arg
+if [[ $1 == '--no-bin-links' ]]; then
+    npm i --registry=https://registry.npm.taobao.org/ --no-bin-links
+else
 
-cnpm i
+    # install global cnpm
+    npm i cnpm -g --registry=https://registry.npm.taobao.org/
+    cnpm i
+fi
--- developing/update.sh
+++ developing/update.sh
@@ -5,5 +5,12 @@ composer update
 
 php artisan migrate
 
-# update npm
-cnpm update
+# check arg
+if [[ $1 == '--no-bin-links' ]]; then
+    npm up --registry=https://registry.npm.taobao.org/ --no-bin-links
+else
+    cnpm up
+fi
+
+# build js and css files
+npm run prod
