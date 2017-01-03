2baa3b17a3ebaef0d25df641abd21eda4ebb1b9d
ttionya <git@ttionya.com>
2017-01-03 16:57:41 +0800

    Update vue post component

--- resources/assets/js/components/Post.vue
+++ resources/assets/js/components/Post.vue
@@ -7,25 +7,28 @@
             </div>
             <article v-else class="post">
                 <header class="post-header">
-                    <h2>{{ mainData.title }}</h2>
+                    <h2>{{ post.title }}</h2>
                 </header>
-                <div class="post-content" v-html="mainData.content"></div>
+                <div class="post-content" v-html="post.content"></div>
                 <hr>
                 <footer class="post-footer">
-                    <span>{{ mainData.updated_at }} Posted by {{ mainData.user_id }}</span>
-                    <br>
-                    <router-link :to="'/category/' + mainData.category_name">{{ mainData.category_name }}</router-link>
-                    <br>
-                    <template v-for="tag in mainData.tags">
+                    <p>
+                        <span>Posted by {{ post.user_id }}</span>
+                        &nbsp;<span v-html="getDate(post.created_at, post.updated_at)"></span>
+                    </p>
+                    <p>
+                        <router-link :to="'/category/' + post.category_name">{{ post.category_name }}</router-link>
+                    </p>
+                    <template v-for="tag in post.tags">
                         <router-link :to="'/tag/' + tag.tag_name">
                             {{ tag.tag_name }}
                         </router-link>
                     </template>
                 </footer>
             </article>
-            <aside v-if="mainData.neighbour">
-                <p v-if="mainData.neighbour.prev">上一篇：<router-link :to="'/blog/' + mainData.neighbour.prev.id">{{ mainData.neighbour.prev.title }}</router-link></p>
-                <p v-if="mainData.neighbour.next">下一篇：<router-link :to="'/blog/' + mainData.neighbour.next.id">{{ mainData.neighbour.next.title }}</router-link></p>
+            <aside v-if="post.neighbour">
+                <p v-if="post.neighbour.prev">上一篇：<router-link :to="'/blog/' + post.neighbour.prev.id">{{ post.neighbour.prev.title }}</router-link></p>
+                <p v-if="post.neighbour.next">下一篇：<router-link :to="'/blog/' + post.neighbour.next.id">{{ post.neighbour.next.title }}</router-link></p>
             </aside>
         </div>
     </div>
@@ -37,7 +40,7 @@
         data() {
             return{
                 error: { },
-                mainData: ''
+                post: ''
             }
         },
         mounted: function() {
@@ -53,7 +56,7 @@
                 self.$http.get('/api' + self.$route.path)
                     .then(data => {
                         self.error = { };
-                        self.mainData = data.body.main;
+                        self.post = data.body.main;
                     })
                     .catch(error => {
                         self.error = {
@@ -61,6 +64,14 @@
                             Text: error.statusText
                         }
                     });
+            },
+            getDate (created_at, updated_at) {
+                if (updated_at) {
+                    return '<span title="Created at ' + created_at + '">Updated at ' + updated_at + '</span>';
+                }
+                else {
+                    return '<span>Created at ' + created_at + '</span>';
+                }
             },
             goBack: () => history.go(-1)
         },
